70e3424ae6b91225cbb65e6c7538aa74035b864d
HenryGlo <43855485+HenryGlo@users.noreply.github.com>
2020-06-07 13:31:23 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+      },
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        }
+      },
+  });
